5c2c4d68b9470343c4b3aa97bb0c7308648fbcb8
Keturah Walters <your.ketwalters@gmail.com>
2013-11-20 00:47:03 -0500

    Add updated files

--- max_solution.rb
+++ max_solution.rb
@@ -2,9 +2,11 @@
 # part I. implement a maximum method that takes and array and returns the largest element of the array
 # part II. reimplement this method as an instance method on the array class
 
+arr = [1,2,3]
 def maximum(arr)
-
+  arr.max 
 end
+maximum(arr)
 
 
 # now rewrite this as a method on the array class
--- square_arrays_solution.rb
+++ square_arrays_solution.rb
@@ -1,6 +1,11 @@
 # Instructions:
 # Return an array that contains the squares of all elements in an array?
 
+some_array = [1,2,3]
 def square_array(some_array)
-  # code goes here
+  some_array.each do |element|
+    element = element * element
+    puts element
 end
+end
+square_array(some_array)
